335b069adf9037d43ef8f6e0c190f84977dbf3e2
Goh Yong Siang Ivan <gyong-si@c1r5s5.42singapore.sg>
2025-07-02 12:17:50 +0800

    edited volumes

--- srcs/setup.sh
+++
@@ -1,8 +0,0 @@
-HOST_ENTRY="10.0.2.15 gyong-si.42.fr"
-
-if ! grep -q "$HOST_ENTRY" /etc/hosts; then
-    echo "Adding $HOST_ENTRY to /etc/hosts"
-    echo "$HOST_ENTRY" | sudo tee -a /etc/hosts > /dev/null
-else
-    echo "$HOST_ENTRY already exists in /etc/hosts"
-fi
